1c5c5dffc6ee0b85b40d107ae4765e7d10632741
THOMAS HARDING <hardingt604@gmail.com>
2022-04-28 16:50:38 -0600

    npm i and added files to be used later

---
+++ src/Parade.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import AnimalEmoji from './AnimalEmoji';
+
+export default function AnimalParade() {
+  const [animals, setAnimals] = useState(['bear', 'tiger', 'alligator']);
+
+  function handleAddBear() {
+    animals.push('bear');
+
+    setAnimals([...animals]);  
+  }
+
+  function handleAddTiger() {
+    animals.push('tiger');
+
+    setAnimals([...animals]);  
+  }
+
+  function handleAddAlligator() {
+    animals.push('alligator');
+
+    setAnimals([...animals]);  
+  }
+  function handleRemoveAnimal() {
+    animals.pop();
+
+    setAnimals([...animals]);  
+  }
+
+  return (
+    <>
+      <div>
+        {animals.map(animal => <AnimalEmoji key={animal} animal={animal} />)}
+      </div>
+      <button onClick={handleAddBear}>Add bear</button>
+      <button onClick={handleAddTiger}>Add tiger</button>
+      <button onClick={handleAddAlligator}>Add alligator</button>
+      <button onClick={handleRemoveAnimal}>Remove last animal</button>
+    </>
+  );
+}
