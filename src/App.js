9d2eaaefef268e198faec6efe3ae00017c6c6cec
THOMAS HARDING <hardingt604@gmail.com>
2022-04-28 16:36:42 -0600

    added initial state and unicorn lion fight

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const ATTACK_VALUE = 1;
+
+function handleLionIncrease() {
+  setLionSize(lionSize + ATTACK_VALUE);
+}
+
+function handleLionAttack() {
+  setUnicornSize(unicornSize - ATTACK_VALUE);
+}
+
+function handleUnicornIncrease() {
+  setUnicornSize(unicornSize + ATTACK_VALUE);
+}
+
+function handleUnicornAttack() {
+  setLionSize(lionSize - ATTACK_VALUE);
+}
+
+
 function App() {
+  const [isOpen, setIsOpen] = useState('false');
+  const [unicornSize, setUnicornSize] = useState(10);
+  const [lionSize, setLionSize] = useState(10);
+  const [animals, setAnimals] = useState('giraffe', 'elephant', 'donkey');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="fight">
+        <div style={{ width: `${unicornSize * 10}px` }} className="monster">
+          <img src="unicorn.png" width={20} />
+          <div className='buttons'>
+            <button onClick={handleUnicornIncrease}>
+              Grow unicorn
+            </button>
+            <button onClick={handleUnicornAttack}>
+              Attack lion
+            </button>
+          </div>
+        </div>
+        <div style={{ width: `${lionSize * 10}px` }} className="monster">
+          <img src="lion.png" width={20} />
+          <div className="buttons">
+            <button onClick={handleLionIncrease}>
+              Grow lion
+            </button>
+            <button onClick={handleLionAttack}>
+              Attack unicorn
+            </button>
+          </div>
+        </div>
+      </div>
       </header>
     </div>
   );
