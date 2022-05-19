a38a8819f8e738f5ec02728c9f4dd0cf03d56799
THOMAS HARDING <hardingt604@gmail.com>
2022-05-19 02:13:56 -0600

    moved the admin path to wghere it's supposed to be and added a redirect router in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
+import AdminPage from './admin/AdminPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import OpenSign from './OpenSign';
 import Parade from './Parade';
 import CustomButton from './CustomButton';
@@ -44,49 +51,71 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="fight">
-          <div className="monster unicorn" >
-            <img src="unicorn.png" width={`${unicornSize * 10}px`} height={`${unicornSize * 10}px`} />
-            <div className='buttons'>
-              <CustomButton onClick={handleUnicornIncrease}>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/admin">
+            <AdminPage />
+          </Route>
+          <Route path="/">
+            <div className="App">
+              <header className="App-header">
+                <div className="fight">
+                  <div className="monster unicorn" >
+                    <img src="unicorn.png" width={`${unicornSize * 10}px`} height={`${unicornSize * 10}px`} />
+                    <div className='buttons'>
+                      <CustomButton onClick={handleUnicornIncrease}>
               Grow unicorn
-              </CustomButton>
-              <CustomButton onClick={handleUnicornAttack}>
+                      </CustomButton>
+                      <CustomButton onClick={handleUnicornAttack}>
               Attack lion
-              </CustomButton>
-            </div>
-          </div>
-          <div className="monster lion">
-            <img src="lion.png" width={`${lionSize * 10}px`} height={`${lionSize * 10}px`} />
-            <div className="buttons">
-              <CustomButton onClick={handleLionIncrease}>
+                      </CustomButton>
+                    </div>
+                  </div>
+                  <div className="monster lion">
+                    <img src="lion.png" width={`${lionSize * 10}px`} height={`${lionSize * 10}px`} />
+                    <div className="buttons">
+                      <CustomButton onClick={handleLionIncrease}>
               Grow lion
-              </CustomButton>
-              <CustomButton onClick={handleLionAttack}>
+                      </CustomButton>
+                      <CustomButton onClick={handleLionAttack}>
               Attack unicorn
-              </CustomButton>
+                      </CustomButton>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div className='open-sign'> 
+                  <OpenSign isOpen={isOpen} />
+                  <div className="open-buttons">
+                    <CustomButton onClick={() => setIsOpen(true)}>Open the zoo</CustomButton>
+                    <CustomButton onClick={() => setIsOpen(false)}>Close it</CustomButton>
+                  </div>
+                </div>
+                <hr />
+                <div className='animal-parade'>
+                  <Parade animals={animals} />
+                  <CustomButton onClick={handleAddWolf}>Wolf</CustomButton>
+                  <CustomButton onClick={handleAddElephant}>Elephant</CustomButton>
+                  <CustomButton onClick={handleAddSnail}>Snail</CustomButton>
+                </div>
+              </header>
             </div>
-          </div>
-        </div>
-        <hr />
-        <div className='open-sign'> 
-          <OpenSign isOpen={isOpen} />
-          <div className="open-buttons">
-            <CustomButton onClick={() => setIsOpen(true)}>Open the zoo</CustomButton>
-            <CustomButton onClick={() => setIsOpen(false)}>Close it</CustomButton>
-          </div>
-        </div>
-        <hr />
-        <div className='animal-parade'>
-          <Parade animals={animals} />
-          <CustomButton onClick={handleAddWolf}>Wolf</CustomButton>
-          <CustomButton onClick={handleAddElephant}>Elephant</CustomButton>
-          <CustomButton onClick={handleAddSnail}>Snail</CustomButton>
-        </div>
-      </header>
-    </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/admin/AdminPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyTable from './MyTable';
+import MyCharts from './MyCharts';
+
+export default function AdminPage() {
+  return (
+    <div>
+        hi
+      <MyTable />
+      <MyCharts />
+    </div>
+  );
+}
